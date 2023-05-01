483c4504199918266ac58b6f3906f6304824e328
Nicole Faulise <nicolef@nursedash.com>
2023-05-01 07:45:29 -0400

    fix ID for staging querying Facility on Me function

--- src/shared/gql/me/queries.ts
+++ src/shared/gql/me/queries.ts
@@ -4,7 +4,7 @@ import allMeFields from './fields';
 
 const getMeFacility = (facilityFields: string = allFacilityFields, meFields: string = allMeFields): DocumentNode => gql`
   query Facility($id: Int!) {
-    Me {
+    Me(id: $id) {
       ... on MeFacility {
         ${facilityFields}
       }
