dc54356208d57ecff265f5eb3ce790d7720d9f6a
Nicole Faulise <nicolef@nursedash.com>
2023-05-16 02:14:23 -0400

    fix favicon

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
         ]
       },
       {
-        path: 'redirect/:redirect/token/:token/facility/:facility',
+        path: 'redirect/:redirect/token/:token/facility/:facility/copy?/:unit?/:shiftType?/:startTime?/:endTime?/:description?/:breakTime?/:qualifications?',
         element: <Redirect />,
       }
     ]
