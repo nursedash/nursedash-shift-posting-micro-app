23603872b82962516c244ee7f6f8995078f423e5
Nicole Faulise <nicolef@nursedash.com>
2023-05-25 12:36:36 -0400

    fix sentry sampling rate

--- src/core/integrations/sentry.ts
+++ src/core/integrations/sentry.ts
@@ -1,22 +1,20 @@
 import * as Sentry from '@sentry/react';
-import { Replay } from '@sentry/react';
 
 const apiUrl = process.env.REACT_APP_API_URL ?? '';
 
 const initSentry = (): void => {
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
-    integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+    integrations: [new Sentry.BrowserTracing(), new Sentry.Replay({
+      networkDetailAllowUrls: [window.location.origin, apiUrl],
+    })],
     tracesSampleRate: .5,
-    replaysSessionSampleRate: .5,
+    replaysSessionSampleRate: 0,
     replaysOnErrorSampleRate: .5,
     environment: process.env.REACT_APP_ENVIRONMENT,
   })
 }
 
-// eslint-disable-next-line no-new
-new Replay({
-  networkDetailAllowUrls: [window.location.origin, apiUrl],
-});
+
 
 export default initSentry;
