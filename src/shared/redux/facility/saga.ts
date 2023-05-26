ac56086e8b8ecec061c26f61d8923c277be840a1
Nicole Faulise <nicolef@nursedash.com>
2023-05-26 18:41:49 -0400

    fallback for if there is no facility Id to be pulled

--- src/shared/redux/facility/saga.ts
+++ src/shared/redux/facility/saga.ts
@@ -11,25 +11,31 @@ import logError from '../../utils/logError';
 export function* watchStoreFacilityDataAsync(): Generator<Effect, void> {
   try {
     const facilityId = (yield select(selectCoreFacilityId)) as number;
-    const variables: GetMeVariables = { id: facilityId };
+    if (facilityId == null || facilityId === 0) {
+      const msg = 'No facility ID present in local storage.';
+      logError(new Error(msg));
+    } else {
+      const variables: GetMeVariables = { id: facilityId };
 
-    // @ts-expect-error
-    const response: ApolloQueryResult<GetMeFacilityData> = yield call(client.query, {
-      query: getMeFacility(),
-      variables
-    });
+      // @ts-expect-error
+      const response: ApolloQueryResult<GetMeFacilityData> = yield call(client.query, {
+        query: getMeFacility(),
+        variables
+      });
 
-    yield put(facilityActions.storeFacilityData({
-      ...response.data.Me,
-      allShiftUnits: response.data.allShiftUnits ,
-      allQualificationTypes: response.data.allQualificationTypes
-    }));
+      yield put(facilityActions.storeFacilityData({
+        ...response.data.Me,
+        allShiftUnits: response.data.allShiftUnits,
+        allQualificationTypes: response.data.allQualificationTypes
+      }));
 
-    Sentry.setUser({
-      id: response?.data?.Me?.id?.toString() ?? '',
-      email: response?.data?.Me?.email ?? ''
-    });
-    yield put(coreActions.setLoadingStatus(false));
+      Sentry.setUser({
+        id: response?.data?.Me?.id?.toString() ?? '',
+        email: response?.data?.Me?.email ?? ''
+      });
+
+      yield put(coreActions.setLoadingStatus(false));
+    }
   } catch (error) {
     yield put(coreActions.setLoadingStatus(false));
     const msg = `There was an error fetching the facility data. Please reload the page.`;
